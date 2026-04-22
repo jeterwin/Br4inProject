796f8984da0551fbe0b6db000f8ca4aa16e7e74c
giurrgiu <alexandru.giurgiu04@e-uvt.ro>
2026-04-22 23:22:56 +0300

    add bullet and enemy shooter scripts

---
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float _speed;
+    private float _age;
+
+    [SerializeField] private float _lifetime = 10f;
+
+    public void Initialize(Vector3 direction, float speed)
+    {
+        transform.rotation = Quaternion.LookRotation(direction);
+        _speed = speed;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        _age += Time.deltaTime;
+        if (_age >= _lifetime)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player hit by bullet!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyShooter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnemyShooter : MonoBehaviour
+{
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private float _fireInterval = 2f;
+    [SerializeField] private float _bulletSpeed = 8f;
+
+    private Transform _playerTransform;
+    private float _timer;
+
+    private void Start()
+    {
+        _playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= _fireInterval)
+        {
+            Fire();
+            _timer = 0f;
+        }
+    }
+
+    private void Fire()
+    {
+        Vector3 direction = (_playerTransform.position - transform.position).normalized;
+        GameObject bulletObj = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
+        bulletObj.GetComponent<Bullet>().Initialize(direction, _bulletSpeed);
+    }
+}
