8853c5fea2641e42ac3854744a9a05a8caf1ebdb
Miruna022 <maria.mesarosiu05@e-uvt.ro>
2026-04-25 20:34:09 +0300

    finished main menu and in-game UI (95%)

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -24,8 +24,6 @@ public class PlayerMovementController : MonoBehaviour
     private void Update()
     {
         if (GameManager.IsPaused) return;
-
-        HandleMouseLook();
         HandleMovement();
     }
 
